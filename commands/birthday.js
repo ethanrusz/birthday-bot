34f54770dccfb481604addeabcfcc4319a10b00c
Ethan Ruszanowski <ethan@rusz.dev>
2022-04-29 01:29:26 -0400

    Add birthday commands

--- commands/birthday.js
+++ commands/birthday.js
@@ -37,11 +37,49 @@ module.exports = {
             .setDescription("Enter a user")
             .setRequired(true)
         )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("update")
+        .setDescription("Updates a birthday")
+        .addUserOption((option) =>
+          option
+            .setName("user")
+            .setDescription("Enter a user")
+            .setRequired(true)
+        )
+        .addIntegerOption((option) =>
+          option.setName("day").setDescription("Enter a day").setRequired(true)
+        )
+        .addIntegerOption((option) =>
+          option
+            .setName("month")
+            .setDescription("Enter a month")
+            .setRequired(true)
+        )
+        .addIntegerOption((option) =>
+          option.setName("year").setDescription("Enter a year")
+        )
     ),
   async execute(interaction) {
-    await interaction.reply({
-      content: "Pong!",
-      ephemeral: true,
-    });
+    if (interaction.options.getSubcommand() === "add") {
+      const user = interaction.options.getUser("user");
+      const day = interaction.options.getInteger("day");
+      const month = interaction.options.getInteger("month");
+      const year = interaction.options.getInteger("year");
+
+      await interaction.reply(`Add ${user} ${year}-${month}-${day}`);
+    } else if (interaction.options.getSubcommand() === "remove") {
+      const user = interaction.options.getUser("user");
+
+      await interaction.reply(`Remove ${user}`);
+    } else if (interaction.options.getSubcommand() === "update") {
+      const user = interaction.options.getUser("user");
+      const day = interaction.options.getInteger("day");
+      const month = interaction.options.getInteger("month");
+      const year = interaction.options.getInteger("year");
+
+      await interaction.reply(`Update ${user} ${year}-${month}-${day}`);
+    }
   },
 };

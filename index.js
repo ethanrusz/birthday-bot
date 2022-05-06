4f180dca289c18b5dce55422f38d236281f73e59
Ethan Ruszanowski <ethan@rusz.dev>
2022-05-06 12:37:22 -0400

    Role job

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require("node:fs");
 const { Client, Collection, Intents } = require("discord.js");
-const { token } = require("./config.json");
+const { token, roleID, guildID } = require("./config.json");
 const cron = require("node-cron");
+const { findByDate } = require("./database.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
@@ -18,8 +19,37 @@ for (const file of commandFiles) {
 client.once("ready", () => {
   console.log("Ready!");
 
-  cron.schedule("0 * * * *", () => {
-    console.log("Cron, yo!");
+  // list all users with birthdays today from findByDate
+  cron.schedule("0 * * * * *", async () => {
+    console.log("Running cron job");
+
+    const guild = client.guilds.cache.get(guildID);
+    const role = guild.roles.cache.get(roleID);
+
+    // remove birthday role from all users
+    guild.members.cache.forEach((member) => {
+      if (member.roles.cache.has(role.id)) {
+        member.roles.remove(role);
+      }
+    });
+
+    //get all users with birthdays today
+    const birthdayIDs = await findByDate(
+      new Date().getDate(),
+      new Date().getMonth() + 1
+    );
+    if (birthdayIDs) {
+      //add role roleID to users in guild guildID
+      const membersWithBirthday = guild.members.cache.filter((member) =>
+        birthdayIDs.includes(member.id)
+      );
+      membersWithBirthday.forEach((member) => {
+        member.roles.add(role);
+        // FIXME: add birthday role to other users
+      });
+    } else {
+      console.log("No birthdays today");
+    }
   });
 });
 

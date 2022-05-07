ee9b08fc2b1317576c8455b1d9b497b32fbb718a
Ethan Ruszanowski <ethan@rusz.dev>
2022-05-07 00:46:00 -0400

    Add and remove roles based on birthday date

--- index.js
+++ index.js
@@ -4,42 +4,68 @@ const { token, roleID, guildID } = require("./config.json");
 const cron = require("node-cron");
 const { findByDate } = require("./database.js");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// Create a new Discord client with intents
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+});
 
+// Collection of all the commands files
 client.commands = new Collection();
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
 
+// Load all the commands
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
 
+// When the bot is ready
 client.once("ready", () => {
   console.log("Ready!");
 
-  // list all users with birthdays today from findByDate
+  // Nightly birthday job
   cron.schedule("0 * * * * *", async () => {
     console.log("Running cron job");
 
-    // FIXME: Remove roles
-    // remove birthday roles from all users
+    // Get guild from guildID
+    const guild = client.guilds.cache.get(guildID);
+    // Get role from roleID
+    const role = guild.roles.cache.get(roleID);
 
-    //get all users with birthdays today
+    // Remove birthday role from all users
+    guild.members.fetch().then((members) => {
+      members.forEach((member) => {
+        if (member.roles.cache.has(role.id)) {
+          member.roles.remove(role.id);
+        }
+      });
+    });
+
+    // Get all users with birthdays today from MongoDB
     const birthdayIDs = await findByDate(
       new Date().getDate(),
-      new Date().getMonth() + 1
+      new Date().getMonth() + 1 // month is 0-indexed
     );
+
+    // If there are birthdays today
     if (birthdayIDs) {
-      //add role roleID to users in guild guildID
-      // TODO: add roles
+      // Add birthday role to all users with birthdays today
+      guild.members.fetch().then((members) => {
+        members.forEach((member) => {
+          if (birthdayIDs.includes(member.id)) {
+            member.roles.add(role.id);
+          }
+        });
+      });
     } else {
       console.log("No birthdays today");
     }
   });
 });
 
+// Command handler
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
 
